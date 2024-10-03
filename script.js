8ee2f3101f69b280047f799fd0e12091ea13650d
NikoWD <mikolaj.wd0821@gmail.com>
2024-10-03 22:59:34 +0200

    build strukture

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log("hello to do list, it's me");
+
+let todoInput
+let errorInfo
+let addBtn
+let ulList
+
+const main = () => {
+    prepareDOMElements()
+    prepareDOMEvents()
+
+}
+
+const prepareDOMElements = () =>{
+    todoInput = document.querySelector('.todo-input')
+    errorInfo = document.querySelector('.error-info')
+    addBtn = document.querySelector('.btn-add')
+    ulList = document.querySelector('.todolist ul')
+    
+}
+
+const prepareDOMEvents = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', main)
