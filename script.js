e947786d6d6278f40d132bdba02a4dfc4f08e465
NikoWD <mikolaj.wd0821@gmail.com>
2024-10-12 01:08:19 +0200

    add function addnew to do

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let todoInput
 let errorInfo
 let addBtn
 let ulList
+let newTodo
 
 const main = () => {
     prepareDOMElements()
     prepareDOMEvents()
-
 }
 
 const prepareDOMElements = () =>{
@@ -16,24 +16,32 @@ const prepareDOMElements = () =>{
     errorInfo = document.querySelector('.error-info')
     addBtn = document.querySelector('.btn-add')
     ulList = document.querySelector('.todolist ul')
-    
 }
+
+
+// 1. tworzy nowy element (li)
+// 2. dodawać nowy element do ul Listy
+// 3. funkcja jodpalana na click w przycisky ADD
+// 4. przychwytuję treść z inputa i umieszcza go s nowoutworzonym LI
+// 5. funkcja nie doda do toodósa pustego inputa
+
 
 const prepareDOMEvents = () => {
-
+    addBtn.addEventListener('click', addNewTodo)
 }
 
-
-
-
-
-
-
-
-
-
-
-
+const addNewTodo = () => {
+    if (todoInput.value !== '') {
+        newTodo = document.createElement('li')
+        newTodo.textContent = todoInput.value
+        console.log(newTodo)
+        ulList.append(newTodo)
+        errorInfo.textContent = ""
+    } else {
+        errorInfo.textContent = "wpisz treść zadania"
+    }
+    
+}
 
 
 
