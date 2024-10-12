c262ab543b67dc58a0b96574587cfefdc3dd2a51
NikoWD <mikolaj.wd0821@gmail.com>
2024-10-13 00:43:14 +0200

    add button + logic them

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ let addBtn
 let ulList
 let newTodo
 
+let popup
+let popupInfo
+let todoToEdit
+let popupInput
+let popupAddBtn
+let popupCloseBtn
+
 const main = () => {
     prepareDOMElements()
     prepareDOMEvents()
@@ -16,32 +23,83 @@ const prepareDOMElements = () =>{
     errorInfo = document.querySelector('.error-info')
     addBtn = document.querySelector('.btn-add')
     ulList = document.querySelector('.todolist ul')
+
+    popup = document.querySelector('.popup')
+    popupInfo = document.querySelector('.popup-info')
+    popupInput = document.querySelector('.popup-input')
+    popupAddBtn = document.querySelector('.accept')
+    popupCloseBtn = document.querySelector('.cancel')
 }
-
-
-// 1. tworzy nowy element (li)
-// 2. dodawać nowy element do ul Listy
-// 3. funkcja jodpalana na click w przycisky ADD
-// 4. przychwytuję treść z inputa i umieszcza go s nowoutworzonym LI
-// 5. funkcja nie doda do toodósa pustego inputa
-
 
 const prepareDOMEvents = () => {
     addBtn.addEventListener('click', addNewTodo)
+    ulList.addEventListener('click', checkClick)
+    popupCloseBtn.addEventListener('click', closePopup)
 }
 
 const addNewTodo = () => {
     if (todoInput.value !== '') {
         newTodo = document.createElement('li')
         newTodo.textContent = todoInput.value
-        console.log(newTodo)
+        createToolsArea()
+
         ulList.append(newTodo)
-        errorInfo.textContent = ""
+
+        todoInput.value = " "
+        errorInfo.textContent = " "
     } else {
         errorInfo.textContent = "wpisz treść zadania"
     }
     
 }
+
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div')
+    toolsPanel.classList.add('tools')
+    newTodo.append(toolsPanel)
+    
+    const completeBtn = document.createElement('button')
+    completeBtn.classList.add('complete')
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+    const editBtn = document.querySelector('button')
+    editBtn.classList.add('edit')
+    editBtn.textContent = 'EDIT'
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class = "fas fa-times"></i>'
+
+    toolsPanel.append(completeBtn, editBtn, deleteBtn)
+
+}
+
+
+const checkClick = (e) => {
+    if (e.target.matches('.complete')){
+        console.log('complete');
+        e.target.closest('li').classList.toggle('completed')
+        e.target.classList.toggle('completed')
+
+    } else if (e.target.matches('.edit')) {
+        console.log('open popup edit to do ');
+        editTodo()
+    } else if (e.target.matches('.delete')){
+        console.log('delete');
+    }
+
+}
+
+
+
+const editTodo = () => {
+    popup.style.display = 'flex'
+}
+
+const closePopup = (params) => {
+    popup.style.display = 'none'
+}
+
 
 
 
